6fef2fcea1376d1e8136839a5396825e7ebee177
michaelik1 <gulkov.mikhail@mail.ru>
2026-08-15 23:42:01 +0300

    test: add keepalive optional flag and generation tests

--- wireguard/profile_test.go
+++ wireguard/profile_test.go
@@ -1,6 +1,9 @@
 package wireguard
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestGenerateProfile(t *testing.T) {
 	var expectedResult = `[Interface]
@@ -27,5 +30,53 @@ Endpoint = 5
 
 	if expectedResult != result {
 		t.Error()
+	}
+}
+
+func TestGenerateProfileWithKeepalive(t *testing.T) {
+	var expectedResult = `[Interface]
+PrivateKey = 1
+Address = 2/32, 3/128
+DNS = 1.1.1.1, 1.0.0.1, 2606:4700:4700::1111, 2606:4700:4700::1001
+MTU = 1280
+[Peer]
+PublicKey = 4
+AllowedIPs = 0.0.0.0/0, ::/0
+Endpoint = 5
+PersistentKeepalive = 25
+`
+
+	result, err := generateProfile(&ProfileData{
+		PrivateKey: "1",
+		Address1:   "2",
+		Address2:   "3",
+		PublicKey:  "4",
+		Endpoint:   "5",
+		Keepalive:  25,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if expectedResult != result {
+		t.Errorf("unexpected profile:\n%s", result)
+	}
+}
+
+func TestGenerateProfileWithCustomKeepalive(t *testing.T) {
+	result, err := generateProfile(&ProfileData{
+		PrivateKey: "1",
+		Address1:   "2",
+		Address2:   "3",
+		PublicKey:  "4",
+		Endpoint:   "5",
+		Keepalive:  60,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if expected := "PersistentKeepalive = 60\n"; !strings.HasSuffix(result, expected) {
+		t.Errorf("expected profile to end with %q, got:\n%s", expected, result)
 	}
 }
